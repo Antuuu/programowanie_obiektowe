d168464e01b82e0f81af93ffcdcda49a929cd463
Antuuu <kacper.cesarz@outlook.com>
2022-04-09 17:16:54 +0200

    Add missing tests

--- lab_01/lab_01_tests/UnitTest1.cs
+++ lab_01/lab_01_tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using lab_01;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -21,4 +22,61 @@ public class FractionTests
         Fraction fraction = new Fraction(licznik,mianownik);  
         Assert.AreEqual(1, fraction.Zaokraglijdogory());
     }
+    [TestMethod]  
+    public void Test_FractionFloor()
+    {
+        Fraction fraction = new Fraction(licznik,mianownik);  
+        Assert.AreEqual(0, fraction.Zaokraglijwdol());
+    }
+    [TestMethod]  
+    public void Test_Product()
+    {
+        Fraction fraction = new Fraction(licznik,mianownik);
+        Fraction fraction2 = new Fraction(mianownik,licznik);
+        Assert.AreEqual("24/24", (fraction * fraction2).ToString());
+    }
+    [TestMethod]  
+    public void Test_Quotient()
+    {
+        Fraction fraction = new Fraction(licznik,mianownik);
+        Fraction fraction2 = new Fraction(mianownik,licznik);
+        Assert.AreEqual("16/36", (fraction / fraction2).ToString());
+    }   
+    [TestMethod]  
+    public void Test_Sum()
+    {
+        Fraction fraction = new Fraction(licznik,mianownik);
+        Fraction fraction2 = new Fraction(mianownik,licznik);
+        Assert.AreEqual("52/24", (fraction + fraction2).ToString());
+    }
+    [TestMethod]  
+    public void Test_Difference()
+    {
+        Fraction fraction = new Fraction(licznik,mianownik);
+        Fraction fraction2 = new Fraction(mianownik,licznik);
+        Assert.AreEqual("-20/24", (fraction - fraction2).ToString());
+    }  
+    [TestMethod]  
+    public void Test_Equals()
+    {
+        Fraction fraction = new Fraction(licznik,mianownik);
+        Fraction fraction2 = new Fraction(mianownik,licznik);
+        Assert.AreEqual(false, fraction.Equals(fraction2));
+    }  
+    [TestMethod]  
+    public void Test_Comparable()
+    {
+            Fraction[] a =
+            {
+                new Fraction(1,10),
+                new Fraction(5,10),
+                new Fraction(6,20),
+                new Fraction(10,8),
+                new Fraction(34,7)
+                
+            };
+        Array.Sort(a);
+        Assert.AreEqual("1/10",a[0].ToString());
+    }  
+    
 }
